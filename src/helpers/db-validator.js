ba40c055a90ec9b66418f1d87579fb7fc9f5ada7
zmenendez-2019484 <zmenendez-2019484@kinal.edu.gt>
2024-03-04 08:49:26 -0600

    Se agregan los middlewares necesarios para el funcionamiento del proyecto

---
+++ src/helpers/db-validator.js
@@ -0,0 +1,15 @@
+import User from '../users/user.model.js';
+
+export const existsEmail = async (email = '') => {
+    const existEmail = await User.findOne({ email });
+    if (existEmail) {
+        throw new Error(`El correo ${email} ya está registrado`);
+    }
+}
+
+export const existsUsername = async (username = '') => {
+    const existUsername = await User.findOne({ username });
+    if (existUsername) {
+        throw new Error(`El usuario ${username} ya está registrado`);
+    }
+}
---
+++ src/middlewares/validate-jwt.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import User from '../users/user.model.js';
+
+export const validateJWT = async (req, res, next) => {
+    const token = req.header('x-token');
+
+    if (!token) {
+        return res.status(401).json({
+            msg: 'No hay token en la petición'
+        });
+    }
+    try {
+        const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+        const user = User.findOne({ uid });
+        if (!user) {
+            return res.status(401).json({
+                msg: 'User if not exists in DB'
+            });
+        }
+        req.user = user;
+        next();
+
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({
+            msg: 'Token no válido'
+        });
+    }
+
+}
