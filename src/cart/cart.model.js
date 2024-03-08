1ab282bebb00df652a9cc8399dd943d47db484db
zmenendez-2019484 <zmenendez-2019484@kinal.edu.gt>
2024-03-08 09:50:06 -0600

    Se crea el modelo de carrito

---
+++ src/cart/cart.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    products: [
+        {
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+            },
+        },
+    ],
+});
+
+export const Cart = mongoose.model("Cart", cartSchema);
